fc89c622221e8d55f2ec9545f7ea88a8be96282a
ldaumail <49167117+ldaumail@users.noreply.github.com>
2021-12-15 13:10:15 -0600

    Add files via upload

--- cohens_d_plots.R
+++ cohens_d_plots.R
@@ -1,12 +1,18 @@
 library(R.matlab)
 library("ggpubr")
 library("colorspace")
+
+setwd("~/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/all_units")
 bumps_data <- readMat("all_data_peaks.mat")
 peaks =bumps_data[["peak.vals"]]
+bfs_data <- readMat("bayesFactors.mat")
+bfs =bfs_data[["bayesfactors"]]
+
 #file_names contains both the filenames and the layer
 file_names_data <- read.csv("filenames_layers.csv", header = TRUE,sep = ',', na.strings="")
 
 #pvalues of adaptation of peak1 vs peak4
+setwd("~/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/lmer_results_peaks")
 pvalues_data <- read.csv("lmer_results_orig_03032020_corrected.csv",header = TRUE,sep = ',', na.strings="")
 
 effectsize<-  array(NaN, c(length(file_names_data[[1]])))
@@ -15,20 +21,20 @@ peak4<- array(NaN, c(length(file_names_data[[1]])))
 
 for(i in 1:length(file_names_data[[1]])){
   if(length(peaks[[i]])>0){
-unit_peaks1 = peaks[[i]][1,]
-unit_peaks4 =peaks[[i]][4,]
-
-mean_pk1 = mean(unit_peaks1)
-mean_pk4 = mean(unit_peaks4)
-
-sd_diff = sd(unit_peaks4-unit_peaks1)
-
-
-cd = (mean_pk4 - mean_pk1)/sd_diff
-
-effectsize[i]=cd
-peak1[i]=mean_pk1
-peak4[i]=mean_pk4
+    unit_peaks1 = peaks[[i]][1,]
+    unit_peaks4 =peaks[[i]][4,]
+    
+    mean_pk1 = mean(unit_peaks1)
+    mean_pk4 = mean(unit_peaks4)
+    
+    sd_diff = sd(unit_peaks4-unit_peaks1)
+    
+    
+    cd = (mean_pk4 - mean_pk1)/sd_diff
+    
+    effectsize[i]=cd
+    peak1[i]=mean_pk1
+    peak4[i]=mean_pk4
   }
 }
 channel_idx = t(t(rep(1:71, times =1)))
@@ -121,14 +127,15 @@ ggplot(all_cd) +
 ###plot just cell class per cell class effect size
 
 #M data
-p_idx =org_data.filt$channel_idx[org_data.filt$layer=="M"]
+#p_idx =org_data.filt$channel_idx[org_data.filt$layer=="M"]
+p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="M"]
 colgrad <-abs(m_percent/100)
 m_data = data.frame(p_idx,colgrad,mcohen)
 
 plot_dat <- melt(m_data, id.var=c('p_idx','colgrad'))
 
 
-  
+
 ggplot(plot_dat) + 
   
   #scale_colour_gradientn(colours=heat.colors(50, alpha=.7, rev = TRUE))+
@@ -177,23 +184,24 @@ kpval = org_data.filt$P.value[org_data.filt$layer=="K"]
 p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="M"]
 colgrad <-abs(m_percent/100)
 m_data = data.frame(p_idx,colgrad,mcohen,mpval)
-
+mmcohen= mean(mcohen, trim = 0)
 #only get significant decreasing
 m_data$sigdata[m_data$mpval<0.05 &mcohen<0] <- "Adapting Decrease"
 m_data$sigdata[m_data$mpval>0.05] <- "Non Adapting"
 m_data$sigdata[m_data$mpval<0.05 &mcohen>0] <- "Adapting Increase"
-  #m_data[ which(m_data$mpval<0.05 & mcohen<0), ]
+#m_data[ which(m_data$mpval<0.05 & mcohen<0), ]
 
 plot_dat <- melt(m_data, id.var=c('p_idx','colgrad','sigdata','mpval'))
 
 ggplot(plot_dat, aes(y=value))+
-    geom_histogram(aes(fill=sigdata),bins=20)+
-    scale_fill_manual(values=c("#93C5DE","#93C5DE","#B5B5B5"))+
-    scale_x_discrete(limits=c(1,2,3,4))+
-    #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
-    #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
-    theme_bw() +
-    theme(
+  geom_histogram(aes(fill=sigdata),bins=20)+
+  scale_fill_manual(values=c("#93C5DE","#93C5DE","#B5B5B5"))+
+  scale_x_discrete(limits=c(1,2,3,4))+
+  geom_hline(aes(yintercept=mmcohen, linetype="Sample Mean"), color = "black")+
+  #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
+  #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
+  theme_bw() +
+  theme(
     plot.title   = element_text(color = "black", size = 15, face = "bold", hjust=0.5),
     axis.title.x = element_text(color = "black", size = 13),
     axis.title.y = element_text(color = "black", size = 13),
@@ -203,13 +211,14 @@ ggplot(plot_dat, aes(y=value))+
     axis.text    = element_text(size  = 9),
     axis.line = element_line(colour = "black", size = 1) 
   )
-    
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/m_cells_cohenD_hist_mean.svg")
+
 #P cells
 
 p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="P"]
 colgrad <-abs(p_percent/100)
 p_data = data.frame(p_idx,colgrad,pcohen,ppval)
-
+mpcohen= mean(pcohen, trim = 0)
 #only get significant decreasing
 p_data$sigdata[p_data$ppval<0.05 &pcohen<0] <- "Adapting Decrease"
 p_data$sigdata[p_data$ppval>0.05] <- "Non Adapting"
@@ -222,6 +231,7 @@ ggplot(plot_dat, aes(y=value))+
   geom_histogram(aes(fill=sigdata),bins=20)+
   scale_fill_manual(values=c("#F6999A","#F6999A","#B5B5B5"))+
   scale_x_discrete(limits=c(1,2,3))+
+  geom_hline(aes(yintercept=mpcohen, linetype="Sample Mean"), color = "black")+
   #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
   #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
   theme_bw() +
@@ -235,12 +245,13 @@ ggplot(plot_dat, aes(y=value))+
     axis.text    = element_text(size  = 9),
     axis.line = element_line(colour = "black", size = 1) 
   )
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/p_cells_cohenD_hist_mean.svg")
 
 #K cells
 p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="K"]
 colgrad <-abs(k_percent/100)
 k_data = data.frame(p_idx,colgrad,kcohen,kpval)
-
+mkcohen= mean(kcohen, trim = 0)
 #only get significant decreasing
 k_data$sigdata[k_data$kpval<0.05 &kcohen<0] <- "Adapting Decrease"
 k_data$sigdata[k_data$kpval>0.05] <- "Non Adapting"
@@ -251,8 +262,9 @@ plot_dat <- melt(k_data, id.var=c('p_idx','colgrad','sigdata','kpval'))
 
 ggplot(plot_dat, aes(y=value))+
   geom_histogram(aes(fill=sigdata),bins=20)+
-  scale_fill_manual(values=c("#A8D5A0","#B5B5B5"))+
+  scale_fill_manual(values=c("#93C5DE","#B5B5B5"))+ #93C5DE #c("#A8D5A0","#B5B5B5"))+
   scale_x_discrete(limits=c(1))+
+  geom_hline(aes(yintercept=mkcohen, linetype="Sample Mean"), color = "black")+
   #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
   #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
   theme_bw() +
@@ -266,4 +278,163 @@ ggplot(plot_dat, aes(y=value))+
     axis.text    = element_text(size  = 9),
     axis.line = element_line(colour = "black", size = 1) 
   )
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/k_cells_cohenD_hist_mean.svg")
+
+## same plots for Bayes Factor ##############################
+
+library(reshape2)
+library(ggplot2)
+#data frame
+
+org_data = data.frame("Unit Index"=channel_idx,"Bayes Factor"=bfs,"Cohen's D"=effectsize,"P-value"=pvalues_data[,5], "Peak1"=peak1,"Peak4"= peak4, file_names_data)
+
+library(tidyr)
+
+row.has.na <- apply(org_data, 1, function(x){any(is.na(x))})
+org_data.filt <- org_data[!row.has.na,]
+
+## all cell classes bayes factors
+mbayes = org_data.filt$Bayes.Factor[org_data.filt$layer=="M"]
+pbayes = org_data.filt$Bayes.Factor[org_data.filt$layer=="P"]
+kbayes = org_data.filt$Bayes.Factor[org_data.filt$layer=="K"]
+
+#pvalues
+mpval = org_data.filt$P.value[org_data.filt$layer=="M"]
+ppval = org_data.filt$P.value[org_data.filt$layer=="P"]
+kpval = org_data.filt$P.value[org_data.filt$layer=="K"]
+
+##percent change
+mpeak1 = org_data.filt$Peak1[org_data.filt$layer=="M"]
+mpeak4 = org_data.filt$Peak4[org_data.filt$layer=="M"]
+m_percent = -100*(mpeak1 - mpeak4)/mpeak1
+
+
+ppeak1 = org_data.filt$Peak1[org_data.filt$layer=="P"]
+ppeak4 = org_data.filt$Peak4[org_data.filt$layer=="P"]
+p_percent = -100*(ppeak1 - ppeak4)/ppeak1
+
+
+kpeak1 = org_data.filt$Peak1[org_data.filt$layer=="K"]
+kpeak4 = org_data.filt$Peak4[org_data.filt$layer=="K"]
+k_percent = -100*(kpeak1 - kpeak4)/kpeak1
+
+## all cell classes effect sizes
+mcohen = org_data.filt$Cohen.s.D[org_data.filt$layer=="M"]
+pcohen = org_data.filt$Cohen.s.D[org_data.filt$layer=="P"]
+kcohen = org_data.filt$Cohen.s.D[org_data.filt$layer=="K"]
+
+##M cells
+
+#t.test(mpeak4, mpeak1, paired = TRUE, alternative = "two.sided")
+#qt(0.975,17)
+
+
+p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="M"]
+#colgrad <-abs(m_percent/100)
+m_data = data.frame(p_idx,log(mbayes),mpval)
+mmbayes= mean(log(mbayes), trim = 0)
+#only get significant decreasing
+m_data$sigdata[m_data$mpval<0.05 &mcohen<0] <- "Adapting Decrease"
+m_data$sigdata[m_data$mpval>0.05] <- "Non Adapting"
+m_data$sigdata[m_data$mpval<0.05 &mcohen>0] <- "Adapting Increase"
+#m_data[ which(m_data$mpval<0.05 & mcohen<0), ]
+
+plot_dat <- melt(m_data, id.var=c('p_idx','sigdata','mpval'))
+
+ggplot(plot_dat, aes(y=value))+
+  geom_histogram(aes(fill=sigdata),bins=20)+
+  scale_fill_manual(values=c("#93C5DE","#93C5DE","#B5B5B5"))+
+  scale_x_discrete(limits=c(1,2,3,4))+
+  geom_hline(aes(yintercept=mmbayes, linetype="Sample Mean"), color = "black")+
+  theme_bw() +
+  theme(
+    plot.title   = element_text(color = "black", size = 15, face = "bold", hjust=0.5),
+    axis.title.x = element_text(color = "black", size = 13),
+    axis.title.y = element_text(color = "black", size = 13),
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text    = element_text(size  = 9),
+    axis.line = element_line(colour = "black", size = 1) 
+  )+
+  labs(
+    title = paste("Bayes Factors \n of M cells"),
+    x = "Unit Count",
+
+    y = "Bayes Factor (log)" )
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/m_cells_bayesfactor_hist_mean.svg")
+
+#P cells
+
+p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="P"]
+colgrad <-abs(p_percent/100)
+p_data = data.frame(p_idx,log(pbayes),ppval)
+mpbayes= mean(log(pbayes), trim = 0)
+#only get significant decreasing
+p_data$sigdata[p_data$ppval<0.05 &pcohen<0] <- "Adapting Decrease"
+p_data$sigdata[p_data$ppval>0.05] <- "Non Adapting"
+p_data$sigdata[p_data$ppval<0.05 &pcohen>0] <- "Adapting Increase"
+#m_data[ which(m_data$mpval<0.05 & mcohen<0), ]
+
+plot_dat <- melt(p_data, id.var=c('p_idx','sigdata','ppval'))
+
+ggplot(plot_dat, aes(y=value))+
+  geom_histogram(aes(fill=sigdata),bins=20)+
+  scale_fill_manual(values=c("#F6999A","#F6999A","#B5B5B5"))+
+  scale_x_discrete(limits=c(1,2,3))+
+  geom_hline(aes(yintercept=mpbayes, linetype="Sample Mean"), color = "black")+
+  #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
+  #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
+  theme_bw() +
+  theme(
+    plot.title   = element_text(color = "black", size = 15, face = "bold", hjust=0.5),
+    axis.title.x = element_text(color = "black", size = 13),
+    axis.title.y = element_text(color = "black", size = 13),
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text    = element_text(size  = 9),
+    axis.line = element_line(colour = "black", size = 1) 
+  )+
+  labs(
+    title = paste("Bayes Factors \n of P cells"),
+    x = "Unit Count",
+    y = "Bayes Factor (log)" )
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/p_cells_bayesfactors_hist_mean.svg")
+
+
+#K cells
+p_idx =org_data.filt$Unit.Index[org_data.filt$layer=="K"]
+k_data = data.frame(p_idx,log(kbayes),kpval)
+mkbayes= mean(log(kbayes), trim = 0)
+#only get significant decreasing
+k_data$sigdata[k_data$kpval<0.05 &kcohen<0] <- "Adapting Decrease"
+k_data$sigdata[k_data$kpval>0.05] <- "Non Adapting"
+k_data$sigdata[k_data$kpval<0.05 &kcohen>0] <- "Adapting Increase"
+
+plot_dat <- melt(k_data, id.var=c('p_idx','sigdata','kpval'))
+
+ggplot(plot_dat, aes(y=value))+
+  geom_histogram(aes(fill=sigdata),bins=20)+
+  scale_fill_manual(values=c("#93C5DE","#B5B5B5"))+ #93C5DE #c("#A8D5A0","#B5B5B5"))+
+  scale_x_discrete(limits=c(1))+
+  geom_hline(aes(yintercept=mkbayes, linetype="Sample Mean"), color = "black")+
+  #geom_hline(yintercept=qt(0.975,18), linetype="dashed", color = "red")
+  #geom_hline(yintercept=-(qt(0.975,9)/sqrt(10)), linetype="dashed", color = "red")
+  theme_bw() +
+  theme(
+    plot.title   = element_text(color = "black", size = 15, face = "bold", hjust=0.5),
+    axis.title.x = element_text(color = "black", size = 13),
+    axis.title.y = element_text(color = "black", size = 13),
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text    = element_text(size  = 9),
+    axis.line = element_line(colour = "black", size = 1) 
+  )+
+  labs(
+    title = paste("Bayes Factors \n of K cells"),
+    x = "Unit Count",
+    y = "Bayes Factor (log)" )
+ggsave(filename= "/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/plots/k_cells_bayesfactors_hist_mean.svg")
 
--- individual_channels_analysis_origpeaks_KRpvalcorr.R
+++ individual_channels_analysis_origpeaks_KRpvalcorr.R
@@ -2,28 +2,34 @@ library(R.matlab)
 library(lmerTest)
 library(car)
 library(lme4)
-
 library(pbkrtest)
-
+source('C:/Users/daumail/OneDrive - Vanderbilt/Documents/R/useful_functions/get_ddf_Lb.R')
+source('C:/Users/daumail/OneDrive - Vanderbilt/Documents/R/useful_functions/rowMaxs.R')
+library(BayesFactor)
 
 ##
 ## create a for loop to treat all the data at once
 
 #file names of all 71 units in the folder
 #ignore for the data analysis
-folderfilenames <-list.files("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/", pattern=NULL, all.files=FALSE,  full.names=FALSE)#ignore
+folderfilenames <-list.files("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/", pattern=NULL, all.files=FALSE,  full.names=FALSE)#ignore
 #ignore for the data analysis
 folderfilenames <- folderfilenames[1:71] #ignore for the data analysis
 
 #file names of all 71 units with cell class label
-filenames <- read.csv("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/all_units/filenames_layers.csv")
+filenames <- read.csv("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/all_units/filenames_layers.csv")
 library(tidyr)
 #keep only labelled units file names
 row.has.na <- apply(filenames, 1, function(x){any(is.na(x))})
 filenames.filt <- filenames[!row.has.na,]
 
 
-data_filename_list <- Sys.glob("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/*.mat")
+#data_filename_list <- Sys.glob("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/*.mat")
+path <- 'C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data_042021/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values/all_units'
+bumpsfile <-file.path(path,"all_data_peaks.mat")
+bumps_data <- readMat(bumpsfile)
+peaks =bumps_data[["peak.vals"]]
+bfs <- array(0, c(length(folderfilenames),1))
 pvals <- array(0, c(length(folderfilenames),4))
 tvals <- 0
 df.KR <- 0
@@ -36,28 +42,29 @@ for(i in 1:length(folderfilenames)){
 #filename <- filenames[i]
 #path <- system.file("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values", package = "R.matlab")
 #pathname <- file.path("C:/Users/daumail/OneDrive - Vanderbilt/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/su_peaks_03032020_corrected/orig_peak_values",folderfilenames[i])
-bumps_data <- readMat(data_filename_list[i])
-  
+#bumps_data <- readMat(data_filename_list[i])
+#bumpsfile <-file.path(path,folderfilenames[i])
+#bumps_data <- readMat(bumpsfile)  
 
-if(length(bumps_data$peaks)>0) { 
+if(length(bumps_data$"peak.vals"[[i]])>0) { 
   
    if(length(filenames$layer[i])>0){ #ignore for the data analysis
    logical_filenames[i] <- TRUE #ignore for the data analysis
    } #ignore for the data analysis
   
-peak1<- t(bumps_data$peaks[1,])
-peak2<- t(bumps_data$peaks[2,])
-peak3<- t(bumps_data$peaks[3,])
-peak4<- t(bumps_data$peaks[4,])
+peak1<- t(bumps_data$"peak.vals"[[i]][1,])
+peak2<- t(bumps_data$"peak.vals"[[i]][2,])
+peak3<- t(bumps_data$"peak.vals"[[i]][3,])
+peak4<- t(bumps_data$"peak.vals"[[i]][4,])
 col_bumps_data <- t(cbind(peak1,peak2,peak3,peak4))
 
 #create labels
 labels <- c("P1","P2","P3", "P4")
-bumpnb <- matrix(rep(labels, each =length(bumps_data$peaks[1,])), ncol=4)
+bumpnb <- matrix(rep(labels, each =length(bumps_data$"peak.vals"[[i]][1,])), ncol=4)
 t_label <- t(cbind(t(bumpnb[,1]),t(bumpnb[,2]),t(bumpnb[,3]),t(bumpnb[,4])))
 
 #create trial index
-trial_idx = t(t(rep(1:length(bumps_data$peaks[1,]), times =4)))
+trial_idx = t(t(rep(1:length(bumps_data$"peak.vals"[[i]][1,]), times =4)))
 
 #create table
 org_data = data.frame(trial_idx, t_label, col_bumps_data)
@@ -70,6 +77,12 @@ coefs <- data.frame(coef(summary(linearPeaks)))
 df.KR <- get_ddf_Lb(linearPeaks, fixef(linearPeaks))
 p.KR <- 2 * (1 - pt(abs(coefs$t.value), df.KR))
 pvals[i,] <- p.KR
+#Compute bayes factor
+## Bayes factor of model against null
+org_data$trial_idx= factor(org_data$trial_idx)
+#factor(org_data$t_label)
+bf = lmBF(col_bumps_data ~ t_label + trial_idx, data = org_data, whichRandom = "trial_idx")#, posterior=TRUE, iterations=10000)
+bfs[i] <- bf
 
 #for(j in 1:4){ 
 #if you wanna consider the distribution normal (not recommended if small sample)
@@ -97,9 +110,9 @@ print(p.KR)
 #print(df.KR)
 #print(p.KR)
 print(pvals)
-#save pvalues as a .mat file
-#filename3 <- paste("C:/Users/maier/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/lmer_results_peaks/lmer_results_02212020", ".mat", sep = "")
-#writeMat(filename3, pvalues = pvals)
+#save Bayes factors as a .mat file
+filenamebf <- paste(path,"/", "bayesFactors.mat", sep = "")
+writeMat(filenamebf, bayesfactors = bfs)
 
 #save pvalues as a .csv file
 filename4 <- paste("C:/Users/daumail/Documents/LGN_data/single_units/inverted_power_channels/good_single_units_data_4bumps_more/new_peak_alignment_anal/lmer_results_peaks/lmer_results_orig_03032020_corrected", ".csv", sep = "")
